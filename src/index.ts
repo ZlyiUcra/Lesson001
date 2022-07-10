db64aa0b26a848c6d7a4e97b5e3a9788108edd61
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-10 18:37:16 +0100

    Corrects gitignore and typization

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const helloMessage = 'Hello Incubator!!!';
+  res.send(helloMessage)
 })
 
 app.listen(port, () => {
