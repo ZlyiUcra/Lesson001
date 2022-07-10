4f8cb4053b15315d5dcb7fc493460beb6f7879b7
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-10 19:15:48 +0100

    Changes port configuration for heroku purposes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, {Request, Response} from 'express';
 
-app.get('/', (req, res) => {
-  const helloMessage = 'Hello Incubator!!!';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  debugger
+  const helloMessage = 'Hello Incubator';
   res.send(helloMessage)
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
