2f9f2a66ed9905f1082a4dad9c3181f0f2622da0
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-17 21:28:26 +0100

    Corrects youtubeUrl errors with soft regex

--- src/helpers/bloggersHelpers.ts
+++ src/helpers/bloggersHelpers.ts
@@ -1,7 +1,8 @@
 import {ErrorMessagesType, errorsMessagesCreator} from "./errorMessagesCreator";
 
 export const isValidUrl = (url: string) => {
-  const check = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  const check = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+  //const check = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   return check.test(url);
 }
 export const errorCreator = (errors: ErrorMessagesType | undefined, name: string, youtubeUrl?: string) => {
