1356348b0b0cde714085d5d9e00baf77a446f80c
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-17 04:08:30 +0100

    Changes title adding trim

--- src/routes/videos-routes.ts
+++ src/routes/videos-routes.ts
@@ -5,7 +5,7 @@ import {inputValidationMiddleware} from "../middlewares/inputValidationMiddlewar
 
 export const videosRouter = Router({});
 
-const titleValidation = body('title')
+const titleValidation = body('title').trim().
   .isLength({min: 0})
   .withMessage('Title must be present and not empty.');
 
