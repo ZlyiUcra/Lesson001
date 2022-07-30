601980ae1f4c5e7b8d1ef5de888ff51d11dbfc6b
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-07-30 12:14:50 +0100

    Corrects bloggerID validation for posts

--- src/helpers/postsHelpers.ts
+++ src/helpers/postsHelpers.ts
@@ -1,13 +1,18 @@
-import {ErrorMessagesType, errorsMessagesCreator} from "./errorMessagesCreator";
+import {ErrorMessagesType, errorsMessagesCreator, ErrorType} from "./errorMessagesCreator";
+import {bloggersService} from "../domain/bloggers-services";
 
-export const postsErrorCreator = (errors: ErrorMessagesType | undefined,
-                                  title?: string,
-                                  shortDescription?: string,
-                                  content?: string,
-                                  bloggerId?: number) => {
-  const baseErrorList = errors?.errorsMessages ? errors.errorsMessages : [];
+
+const baseErrorList = (errors: ErrorMessagesType | undefined): ErrorType[] =>
+  errors?.errorsMessages ? errors.errorsMessages : [];
+
+export const postsErrorCreator = async (errors: ErrorMessagesType | undefined,
+                                        title?: string,
+                                        shortDescription?: string,
+                                        content?: string,
+                                        bloggerId?: number) => {
+
   if (!title || title.trim().length === 0 || title.length > 30) {
-    errors = errorsMessagesCreator(baseErrorList,
+    errors = errorsMessagesCreator(baseErrorList(errors),
       "Title must be present and contain corresponding quantity of characters",
       "title"
     );
@@ -16,7 +21,7 @@ export const postsErrorCreator = (errors: ErrorMessagesType | undefined,
     shortDescription.trim().length === 0 ||
     shortDescription &&
     shortDescription.length > 30) {
-    errors = errorsMessagesCreator(errors?.errorsMessages ? errors.errorsMessages : [],
+    errors = errorsMessagesCreator(baseErrorList(errors),
       "ShortDescription not correct",
       "shortDescription");
   }
@@ -24,9 +29,22 @@ export const postsErrorCreator = (errors: ErrorMessagesType | undefined,
     content.trim().length === 0 ||
     content &&
     content.length > 30) {
-    errors = errorsMessagesCreator(errors?.errorsMessages ? errors.errorsMessages : [],
+    errors = errorsMessagesCreator(baseErrorList(errors),
       "Content not correct",
       "content");
+  }
+
+  if (typeof bloggerId !== "number") {
+    errors = errorsMessagesCreator(baseErrorList(errors),
+      "BloggerId must be present and correct",
+      "bloggerId")
+  } else {
+    const blogger = await bloggersService.findById(bloggerId)
+    if(!blogger){
+      errors = errorsMessagesCreator(baseErrorList(errors),
+        "BloggerId must be present and correct",
+        "bloggerId")
+    }
   }
   return errors;
 }
