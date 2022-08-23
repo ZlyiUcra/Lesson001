be85ff58d79e9eca0cb7fc6f8b48c2f27803a95e
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-08-23 21:58:17 +0100

    revert some changes in auth middleware

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 import {settings} from "../settings";
 
 export const emailAdapter = {
-  sendEmail: async (email: string, subject: string, message: string) => {
-    let transporter = await nodemailer.createTransport({
+  sendEmail:  (email: string, subject: string, message: string) => {
+    let transporter = nodemailer.createTransport({
       service: settings.EMAIL_SERVICE,
       auth: {
         user: settings.EMAIL,
@@ -11,7 +11,7 @@ export const emailAdapter = {
       }
     });
 
-    let info = await transporter.sendMail({
+    let info =  transporter.sendMail({
       from: `Ya 👻"  <no-reply@email.co>`, // sender address
       to: email, // list of receivers
       subject: subject, // Subject line
