60729f643aff16bf16de70afc6802d330d171082
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-09-09 00:37:23 +0100

    Correts one more time likes' middleware v6

--- src/middlewares/commentLikes/commentLikesMiddleware.ts
+++ src/middlewares/commentLikes/commentLikesMiddleware.ts
@@ -12,7 +12,7 @@ import {likesAuthValidator} from "../../helpers/likes/likesHelper";
 export const commentLikesAuthMiddleware = async (req: RequestWithFullUser, res: Response, next: NextFunction) => {
 
   const {headerAuth, accessToken, userJWT, user, isBearer} = await likesAuthValidator(req.headers.authorization)
-  if (!isBearer || !userJWT) return res.status(401).send()
+  if (!isBearer || accessToken.length < 50) return res.status(401).send()
   // if(!user){
   //   return res.status(401).send()
   // }
