d08081fdc0e3d0d637355f8b37536578f416d20b
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-09-08 23:43:50 +0100

    Correts one more time likes' middleware

--- src/middlewares/commentLikes/commentLikesMiddleware.ts
+++ src/middlewares/commentLikes/commentLikesMiddleware.ts
@@ -13,7 +13,7 @@ import {likesAuthMiddleware} from "../../helpers/likes/likesHelper";
 export const commentLikesAuthMiddleware = async (req: RequestWithFullUser, res: Response, next: NextFunction) => {
 
   const  {headerAuth, accessToken, userJWT, user} = await likesAuthMiddleware(req.headers.authorization)
-  if(!headerAuth) return res.status(401).send()
+  if(!headerAuth || !userJWT) return res.status(401).send()
   // if(!user){
   //   return res.status(401).send()
   // }
--- src/middlewares/postLikes/postLikesMiddleware.ts
+++ src/middlewares/postLikes/postLikesMiddleware.ts
@@ -11,7 +11,7 @@ import {likesAuthMiddleware} from "../../helpers/likes/likesHelper";
 export const postLikesAuthMiddleware = async (req: RequestWithFullUser, res: Response, next: NextFunction) => {
 
   const  {headerAuth, accessToken, userJWT, user} = await likesAuthMiddleware(req.headers.authorization)
-  if(!headerAuth) return res.status(401).send()
+  if(!headerAuth || !userJWT) return res.status(401).send()
   // if(!user){
   //   return res.status(401).send()
   // }
