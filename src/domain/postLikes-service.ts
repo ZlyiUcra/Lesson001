3c8662720b665b0a69a7ead94cbaf27b266b2818
ZlyiUcra <oleksandr.gochu@gmail.com>
2022-09-10 21:59:56 +0100

    Corrects mystatus implementation

--- src/domain/postLikes-service.ts
+++ src/domain/postLikes-service.ts
@@ -12,7 +12,7 @@ export const postLikesService = {
     let postLike: PostLikeType = {
       id: uuidv4(),
       postId,
-      likeStatus: LIKE_STATUS.NONE,
+      likeStatus: likeStatus,
       userId,
       login,
       addedAt: new Date()
@@ -21,7 +21,6 @@ export const postLikesService = {
     if (user) {
       postLike.userId = user.id;
       postLike.login = user.credentials.login;
-      postLike.likeStatus = likeStatus;
       savedPostLike = await this.findByPostIdAndUserId(postId, user.id);
 
     } else {
